971e01d367dfef80492c003b1ed9bc9443eb21a9
autoblack <mjirik@users.noreply.github.com>
2022-09-09 06:11:26 +0000

    fixup: Format Python code with Black

--- tests/test_coco.py
+++ tests/test_coco.py
@@ -4,5 +4,7 @@ from animaltracking import coco_dataset
 
 
 def test_coco_split():
-    cocod = coco_dataset.CocoDataset(Path("instances_default.json"), Path("images_test"))
+    cocod = coco_dataset.CocoDataset(
+        Path("instances_default.json"), Path("images_test")
+    )
     cocod.train_test_split("cctr.json", "ccte.json")
