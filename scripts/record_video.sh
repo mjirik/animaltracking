c5f07d8be9b5a9300b3e5024f754e1c5c4ddc3d4
Miroslav Jirik <mjirik@kky.zcu.cz>
2022-09-15 16:30:53 +0200

    fix typo

--- scripts/record_video.sh
+++ scripts/record_video.sh
@@ -3,5 +3,5 @@
 # sudo apt-get install sshfs livemedia-utils
 
 mkdir -p ~/mnt/biomedical
-sshfs mjirik@storage-plzen4.kky.zcu.cz:/data-ntis/projects/korpusy_cv/biomedical remote_directory ~/mnt/biomedical
+sshfs mjirik@storage-plzen4.kky.zcu.cz:/data-ntis/projects/korpusy_cv/biomedical ~/mnt/biomedical
 
