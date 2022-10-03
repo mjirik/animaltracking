6c23473b6c8b7de6c1ff9a7d843e6b25c9b12385
autoblack <mjirik@users.noreply.github.com>
2022-10-03 11:35:30 +0000

    fixup: Format Python code with Black

--- animaltracking/visualization.py
+++ animaltracking/visualization.py
@@ -6,7 +6,12 @@ from typing import Optional
 from loguru import logger
 import skimage
 
-def create_heatmap_report(points:np.ndarray, image:Optional[np.ndarray]=None, filename:Optional[Path]=None):
+
+def create_heatmap_report(
+    points: np.ndarray,
+    image: Optional[np.ndarray] = None,
+    filename: Optional[Path] = None,
+):
     """
     :param points: xy points with shape = [i,2]
     :param image: np.ndarray with image
@@ -28,15 +33,15 @@ def create_heatmap_report(points:np.ndarray, image:Optional[np.ndarray]=None, fi
     plt.axis("off")
 
     plt.gca().set_axis_off()
-    plt.subplots_adjust(top=1, bottom=0, right=1, left=0,
-                        hspace=0, wspace=0)
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
     plt.margins(0, 0)
     plt.gca().xaxis.set_major_locator(plt.NullLocator())
     plt.gca().yaxis.set_major_locator(plt.NullLocator())
 
     x, y = points[:, 0], points[:, 1]
     sns.kdeplot(
-        x=x, y=y,
+        x=x,
+        y=y,
         fill=True,
         # thresh=0.1,
         # levels=100,
@@ -45,12 +50,12 @@ def create_heatmap_report(points:np.ndarray, image:Optional[np.ndarray]=None, fi
         # palette="jet",
         # cmap="crest",
         cmap="rocket",
-        alpha=.5,
-        linewidth=0
+        alpha=0.5,
+        linewidth=0,
     )
     if filename is not None:
         # plt.savefig(Path(filename))
-        plt.savefig(Path(filename), bbox_inches='tight', pad_inches=0)
+        plt.savefig(Path(filename), bbox_inches="tight", pad_inches=0)
         plt.close(fig)
 
     return fig
