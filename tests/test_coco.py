9f4134b1e744410436c1d232a43a6f9a50ddd59c
Miroslav Jirik <miroslav.jirik@gmail.com>
2023-01-09 16:23:14 +0100

    check skip on CI

--- tests/test_coco.py
+++ tests/test_coco.py
@@ -1,7 +1,11 @@
 import pytest
 from pathlib import Path
 from animaltracking import coco_dataset
+import os
 
+CI = os.getenv("CARNIVOREID_DATASET_BASEDIR", r"H:\biology\orig\CarnivoreID")
+print(type(CI))
+print(CI)
 
 def test_coco_split():
     fnin = Path("instances_default.json")
@@ -14,3 +18,8 @@ def test_coco_split():
 
     cocod = coco_dataset.CocoDataset(fntmp, Path("images_test"))
     cocod.train_test_split(fntest, fnval, 0.5)
+
+
+@pytest.mark.skipif(CI, "This is not possible on CI")
+def test_skip():
+    assert False
